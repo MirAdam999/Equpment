c91d6148c7644662ea232e35c643471600be4589
MirShukhman <miriysha@gmail.com>
2025-02-02 18:06:17 +0200

    deploy

--- equpment-frontend/src/comps/context/URL.js
+++ equpment-frontend/src/comps/context/URL.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext } from 'react';
 const URLContext = createContext();
 
 const URLProvider = ({ children }) => {
-    const storedURL = 'https://8582-2a06-c701-440c-400-14e5-b2c9-966f-ab38.ngrok-free.app';
+    const storedURL = 'https://d6fc-2a06-c701-4421-7b00-3cb0-41e5-b9ba-51ff.ngrok-free.app';
 
     return (
         <URLContext.Provider
